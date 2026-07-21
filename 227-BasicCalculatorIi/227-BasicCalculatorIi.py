ad0e19153677e22778250b0b258c712a409cecdc
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:09:28 +0530

    Create 227-BasicCalculatorIi

---
+++ 227-BasicCalculatorIi/227-BasicCalculatorIi.py
@@ -0,0 +1,25 @@
+# Last updated: 7/21/2026, 7:09:26 PM
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = '+'
+
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            if ch in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    stack.append(int(stack.pop() / num))
+
+                sign = ch
+                num = 0
+
+        return sum(stack)
