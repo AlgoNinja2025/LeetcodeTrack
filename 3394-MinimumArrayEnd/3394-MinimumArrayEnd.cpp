5356a0537a3fc55d342e5c466be8f71ca0b501ad
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:07:35 +0530

    Create 3394-MinimumArrayEnd

---
+++ 3394-MinimumArrayEnd/3394-MinimumArrayEnd.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/21/2026, 7:07:32 PM
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+     long long ans = x;
+        if (n == 1)
+            return ans;
+        --n;
+        vector<int> ref;
+        for (long long i = 0; i < 64; i++) {
+            if (!(ans & (1ll << i)))
+                ref.push_back(i);
+        }
+        for (int i = 0; n; i++, n >>= 1) {
+            ans += (n & 1ll) << ref[i];
+        }
+
+        return ans;   
+    }
+};
