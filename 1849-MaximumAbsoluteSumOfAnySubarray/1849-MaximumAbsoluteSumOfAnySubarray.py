af1b22a78f6a9bc571c0f0fdfd16af30ed7da79f
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:08:41 +0530

    Create 1849-MaximumAbsoluteSumOfAnySubarray

---
+++ 1849-MaximumAbsoluteSumOfAnySubarray/1849-MaximumAbsoluteSumOfAnySubarray.py
@@ -0,0 +1,17 @@
+# Last updated: 7/21/2026, 7:08:39 PM
+class Solution(object):
+    def maxAbsoluteSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        minSum=0
+        maxSum=0
+        prefixSum=0
+
+        for i in range(0, len(nums)):
+            prefixSum += nums[i]
+            minSum=min(minSum, prefixSum)
+            maxSum=max(maxSum, prefixSum)
+        
+        return maxSum-minSum
