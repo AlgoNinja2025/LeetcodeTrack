095ae75e68898890ccafaa5ea26b99732d5e5ffa
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:08:21 +0530

    Create 2170-CountNumberOfMaximumBitwiseOrSubsets

---
+++ 2170-CountNumberOfMaximumBitwiseOrSubsets/2170-CountNumberOfMaximumBitwiseOrSubsets.py
@@ -0,0 +1,17 @@
+# Last updated: 7/21/2026, 7:08:19 PM
+class Solution(object):
+    def countMaxOrSubsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0] * (1 << 17)
+        dp[0] = 1
+        max_or = 0
+
+        for num in nums:
+            for i in range(max_or, -1, -1):
+                dp[i | num] += dp[i]
+            max_or |= num
+
+        return dp[max_or]
