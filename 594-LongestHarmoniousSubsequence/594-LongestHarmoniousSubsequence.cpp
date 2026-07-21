4faab0df2b19c62f35e187699843551999c9a196
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:09:14 +0530

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/21/2026, 7:09:12 PM
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int left=0, max_len=0;
+        for(int right =0;right<nums.size();++right){
+            while(nums[right]-nums[left]>1){
+                ++left;
+            }
+            if(nums[right]-nums[left]==1){
+                max_len=max(max_len,right-left+1);
+            }
+        }
+        return max_len;
+        
+    }
+};
