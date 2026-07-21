77f5ec374873f8a8f19cd423e7d1d770e09bd828
Basanta <basantamukherjee2005@gmail.com>
2026-07-21 19:09:18 +0530

    Create 368-LargestDivisibleSubset

---
+++ 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
@@ -0,0 +1,24 @@
+# Last updated: 7/21/2026, 7:09:16 PM
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        dp = [1] * len(nums)
+        prev = [-1] * len(nums)
+        maxi = 0
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > dp[maxi]:
+                maxi = i
+        res = []
+        i = maxi
+        while i >= 0:
+            res.append(nums[i])
+            i = prev[i]
+        return res
